307f6794c4972537652e734eb5924c48b62ed415
Laurence Billingham <laurence.billingham@gmail.com>
2024-12-26 16:47:37 +0000

    fix: :hourglass: wait for DB to be up before adding user

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,10 +15,21 @@ CONTAINER_NAME="postgres"
 docker run \
     --env POSTGRES_USER=${SUPERUSER} \
     --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
     --publish "${DB_PORT}:5432" \
     --detach \
     --name "${CONTAINER_NAME}" \
     postgres -N 1000
+
+# wait until postgres is ready to accept connections
+until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == "healthy" \
+]; do
+>&2 echo "Postgres is still unavailable - sleeping" sleep 1
+done
 
 # create the non-root user and allow them access to the DB
 CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
